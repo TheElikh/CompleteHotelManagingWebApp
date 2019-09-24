fafe7db14d03313f7a19a2f9a5d12ce8e91d0ba7
TheElikh <md.eliaskanchon.mek@gmail.com>
2019-09-24 14:47:00 +0600

    Room controller

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelManagementSystem.Models;
+using HotelManagementSystem.Models.ViewModel;
+
+namespace HotelManagementSystem.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly HotelContext _context;
+
+        public RoomController(HotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Room
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rooms.Where(m => m.delete == false).ToListAsync());
+        }
+
+        // GET: Room/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewBag.message = null;
+            if (id == null)
+            {
+                ViewBag.message = "Not Found";
+                return NotFound();
+            }
+
+            RoomDetailsViewModel RoomDetail = new RoomDetailsViewModel();
+            RoomDetail.RoomModel = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (RoomDetail.RoomModel == null)
+            {
+                ViewBag.message = "Not Found";
+                return NotFound();
+            }
+
+            id = RoomDetail.RoomModel.RoomTypeId;
+            RoomDetail.RoomTypeViewModel = await _context.RoomType
+                .FirstOrDefaultAsync(m => m.Id == id);
+                
+            if (RoomDetail.RoomTypeViewModel == null)
+            {
+                ViewBag.message = "Not Found";
+                return NotFound();
+            }
+
+            return View(RoomDetail);
+        }
+
+        // GET: Room/Create
+        public IActionResult Create()
+        {
+            ViewBag.roomType = _context.RoomType.ToList();
+            return View();
+        }
+
+        // POST: Room/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoomName,BuildingNo,RoomTypeId,StatusId")] RoomModel roomModel)
+        {
+            ViewBag.roomType = _context.RoomType.ToList();
+            if (ModelState.IsValid)
+            {
+                _context.Add(roomModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roomModel);
+        }
+
+        // GET: Room/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roomModel = await _context.Rooms.FindAsync(id);
+            if (roomModel == null)
+            {
+                return NotFound();
+            }
+            return View(roomModel);
+        }
+
+        // POST: Room/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RoomName,BuildingNo,StatusId")] RoomModel roomModel)
+        {
+            if (id != roomModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(roomModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomModelExists(roomModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roomModel);
+        }
+
+        // GET: Room/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roomModel = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (roomModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(roomModel);
+        }
+
+        // POST: Room/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var roomModel = await _context.Rooms.FindAsync(id);
+            roomModel.delete = true;
+            _context.Update(roomModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomModelExists(int id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
+    }
+}
